60bdd5cb6979085076cb2dc0d24d0f47aac9bba4
Ivan Llamas <ip.llamas13@gmail.com>
2022-02-22 03:49:37 -0800

    wrap initialization code in createGrid function

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 const body = document.querySelector('body');
 const html = document.querySelector('html');
 
-const gridDiv = document.createElement('div');
-gridDiv.classList.add('gridDiv');
-body.appendChild(gridDiv);
-for (let i=0; i<16; i++) {
-  console.log(i);
-  const div = document.createElement('div');
-  div.classList.add('row-container');
-  gridDiv.appendChild(div);
-  for (let j=0; j<16; j++) {
-    const divCell = document.createElement('div');
-    divCell.classList.add('cell');
-    div.appendChild(divCell);
-  }
-}
-
-const cells = document.querySelectorAll('.cell');
-cells.forEach((cell) => cell.addEventListener('mouseover', hoverOver));
-
+createGrid(16); 
 
 function hoverOver(e){
   const cell = e.target;
   console.log(cell);
   cell.classList.add('hover');
+}
+
+function createGrid(cellsPerSide){
+  const gridDiv = document.createElement('div');
+  gridDiv.classList.add('gridDiv');
+  body.appendChild(gridDiv);
+  for (let i=0; i<cellsPerSide; i++) {
+    console.log(i);
+    const div = document.createElement('div');
+    div.classList.add('row-container');
+    gridDiv.appendChild(div);
+    for (let j=0; j<cellsPerSide; j++) {
+      const divCell = document.createElement('div');
+      divCell.classList.add('cell');
+      div.appendChild(divCell);
+    }
+  }
+
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell) => cell.addEventListener('mouseover', hoverOver));
 }
